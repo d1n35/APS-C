a9b96d4a1794063672d129f6e52bd72cf6178471
Aspire <iriamu.925@gmail.com>
2024-10-23 17:31:59 -0300

    finalização da base

--- base.c
+++ base.c
@@ -1,25 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+//Método de ordenação (única parte que será alterada do código original vai ser alguns nomes de varáveis, aqui sendo o VET e SIZE)
+void insert(int *vet, int size){
+    register int a, b; 
+    int t;
+    for(a=1; a<size; a++){
+        t = vet[a];
+        for(b=a-1; b>=0 && t<vet[b]; b--) 
+            vet[b+1] = vet[b];
+        vet[b+1] = t;
+    }
+}
+
 int main(void){
-    FILE *ptrArq;
-    ptrArq = fopen("dtaleat1kdup0.txt","rb");
+
+//Declarando as variáveis
     int vet[10000];
     int size = 0;
     int value = 0;
-    while(fscanf(ptrArq, "%i", &value) == 1)
-        vet[size++] = value;
-    for(int i = 0; 1 < size; i++)
-        printf("%i \n", vet[i]);
-    puts("");
-}
 
-void insert(char *item, int count){
-   register int a, b; 
-   char t;
-   for(a=1; a<count; a++){
-      t = item[a];
-      for(b=a-1; b>=0 && t<item[b]; b--) 
-         item[b+1] = item[b];
-      item[b+1] = t;
-      }
+//Abrindo a base de dados
+    FILE *ptrArq;
+    ptrArq = fopen("dtaleat1kdup0.txt","rb");
+    if (ptrArq == NULL){
+        printf("Erro ao tentar abrir o arquivo!");
+        exit(1);
+    }
+
+//Inserindo a base de dados no vetor    
+    while(fscanf(ptrArq, "%i", &value) == 1){
+        vet[size++] = value;
+    }
+
+//Mostrando o vetor desordenado
+    printf("Valores desordenados:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", vet[i]);
+    }
+
+//Contagem de tempo e ordenação
+    clock_t tempo = clock();
+    insert(vet, size);
+    clock_t tempo2 = clock();
+    
+//Mostrando o vetor ordenado
+    for (int i = 0; i < size; i++) {
+        printf("%d ", vet[i]);
+    }
+
+//Mostra o tempo gasto
+    float r = tempo2 - tempo;
+    printf("\n%d %d", tempo, tempo2);
+    printf("\nclock: %f", r / CLOCKS_PER_SEC);
 }
