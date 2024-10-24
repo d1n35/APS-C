4907c80a0fb0b0d0fb1e845567746c21d2210f52
Aspire <iriamu.925@gmail.com>
2024-10-24 17:51:41 -0300

    inserindo switch (incompleto)

--- base.c
+++ base.c
@@ -20,10 +20,133 @@ int main(void){
     int vet[10000];
     int size = 0;
     int value = 0;
+    int dataSize;
+    int duplicate;
+    int dbForm;
 
 //Abrindo a base de dados
     FILE *ptrArq;
+
+    printf("\nEscolha o tamanho da base que deseja testar:\n1 - 10 dados\n2 - 50 dados\n3-100\n4 - 500 dados\n5 - 1k dados\n6 - 5k dados\n7 - 10k dados\n8 - 50k dados\n9 - 100k dados");
+    scanf("%d", dataSize);
+    switch (dataSize){
+        case 1:
+            printf("\nDeseja que a base tenha dados repetidos?\n1 - Sim\n2 - Não");
+            scanf("%d", duplicate);
+            switch (duplicate){
+            case 1:
+                printf("\nDeseja que a base de dados em sua forma bruta esteja de forma:\n1 - Aleatória\n2 - Concavoa\n3 - Convexa\n4 - Crescente\n5 - Decrescente");
+                scanf("%d", dbForm);
+                switch (dbForm){
+                case 1/* constant-expression */:
+                    /* code */
+                    break;
+                
+                default:
+                    break;
+                }
+                /* code */
+                break;
+            
+            default:
+                break;
+            }
+        break;
+
+        case 2:
+            printf("\nDeseja que a base tenha dados repetidos?\n1 - Sim\n2 - Não");
+            scanf("%d", duplicate);
+            switch (duplicate){
+            case 1:
+                printf("\nDeseja que a base de dados em sua forma bruta esteja de forma:\n1 - Aleatória\n2 - Concavoa\n3 - Convexa\n4 - Crescente\n5 - Decrescente");
+                scanf("%d", dbForm);
+                switch (dbForm){
+                case 1/* constant-expression */:
+                    /* code */
+                    break;
+                
+                default:
+                    break;
+                }
+                /* code */
+                break;
+    
+            default:
+                break;
+        }
+
+        case 3:
+            printf("\nDeseja que a base tenha dados repetidos?\n1 - Sim\n2 - Não");
+            scanf("%d", duplicate);
+            switch (duplicate){
+            case 1:
+                printf("\nDeseja que a base de dados em sua forma bruta esteja de forma:\n1 - Aleatória\n2 - Concavoa\n3 - Convexa\n4 - Crescente\n5 - Decrescente");
+                scanf("%d", dbForm);
+                switch (dbForm){
+                case 1/* constant-expression */:
+                    /* code */
+                    break;
+                
+                default:
+                    break;
+                }
+                /* code */
+                break;
+    
+            default:
+                break;
+        }
+
+        case 4:
+            printf("\nDeseja que a base tenha dados repetidos?\n1 - Sim\n2 - Não");
+            scanf("%d", duplicate);
+            switch (duplicate){
+            case 1:
+                printf("\nDeseja que a base de dados em sua forma bruta esteja de forma:\n1 - Aleatória\n2 - Concavoa\n3 - Convexa\n4 - Crescente\n5 - Decrescente");
+                scanf("%d", dbForm);
+                switch (dbForm){
+                case 1/* constant-expression */:
+                    /* code */
+                    break;
+                
+                default:
+                    break;
+                }
+                /* code */
+                break;
+    
+            default:
+                break;
+        }
+
+        case 5:
+            printf("\nDeseja que a base tenha dados repetidos?\n1 - Sim\n2 - Não");
+            scanf("%d", duplicate);
+            switch (duplicate){
+            case 1:
+                printf("\nDeseja que a base de dados em sua forma bruta esteja de forma:\n1 - Aleatória\n2 - Concavoa\n3 - Convexa\n4 - Crescente\n5 - Decrescente");
+                scanf("%d", dbForm);
+                switch (dbForm){
+                case 1/* constant-expression */:
+                    /* code */
+                    break;
+                
+                default:
+                    break;
+                }
+                /* code */
+                break;
+    
+            default:
+                break;
+        }
+
+    
+    default:
+        break;
+    }
     ptrArq = fopen("dtaleat1kdup0.txt","rb");
+
     if (ptrArq == NULL){
         printf("Erro ao tentar abrir o arquivo!");
         exit(1);
@@ -53,5 +176,5 @@ int main(void){
 //Mostra o tempo gasto
     float r = tempo2 - tempo;
     printf("\n%d %d", tempo, tempo2);
-    printf("\nclock: %f", r / CLOCKS_PER_SEC);
+    printf("\nclock: %2.10f", r / CLOCKS_PER_SEC);
 }
